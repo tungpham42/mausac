39e17786ecd8fc49ff35df6112cf677699ef4ed7
Tung Pham <tung.42@gmail.com>
2025-06-07 08:35:09 +0700

    card

--- app/[color]/not-found.tsx
+++ app/[color]/not-found.tsx
@@ -2,10 +2,10 @@ import ColorSearchForm from "@/components/ColorSearchForm";
 
 export default function NotFound() {
   return (
-    <div className="not-found">
+    <div className="not-found container my-4">
+      <h1 className="mb-4">404 - Không tìm thấy màu</h1>
       <div className="not-found-content">
-        <h2>Color Not Found</h2>
-        <p>The color you entered is invalid or unsupported.</p>
+        <p>Mã màu sai hoặc không hỗ trợ.</p>
         <ColorSearchForm />
       </div>
     </div>
--- components/ColorCard.tsx
+++ components/ColorCard.tsx
@@ -4,7 +4,14 @@ import { Card } from "react-bootstrap";
 export default function ColorCard({ hex }: { hex: string }) {
   return (
     <Card className="my-3">
-      <Card.Body style={{ backgroundColor: hex, height: "100px" }} />
+      <Card.Body
+        style={{
+          backgroundColor: hex,
+          height: "100px",
+          borderBottomLeftRadius: "8px",
+          borderBottomRightRadius: "8px",
+        }}
+      />
     </Card>
   );
 }
