c49ed688f927953d692e4e588fdd95181c106bf9
Tung Pham <tung.42@gmail.com>
2025-08-14 14:27:48 +0700

    <SpeedInsights/>

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import GoogleAnalytics from "@/components/GoogleAnalytics";
 import MainBrandLogo from "@/components/MainBrandLogo";
 import { LanguageProvider } from "@/context/LanguageContext";
 import Script from "next/script";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default async function RootLayout({
   children,
@@ -27,6 +28,7 @@ export default async function RootLayout({
             src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3585118770961536`}
             crossOrigin="anonymous"
           />
+          <SpeedInsights />
         </head>
         <body>
           <MainBrandLogo
