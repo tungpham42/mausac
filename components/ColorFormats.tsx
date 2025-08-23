fd7c6c5d4cb26ffcbf2ff893d00ff6a46f780df7
Tung Pham <tung.42@gmail.com>
2025-08-23 08:49:36 +0700

    Update ColorFormats.tsx

--- components/ColorFormats.tsx
+++ components/ColorFormats.tsx
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faList } from "@fortawesome/free-solid-svg-icons";
 import { useContext } from "react";
 import { LanguageContext } from "@/context/LanguageContext";
+import AdSenseBanner from "./AdSenseBanner";
 
 type Props = {
   formats: {
@@ -18,29 +19,32 @@ export default function ColorFormats({ formats }: Props) {
   const { t } = useContext(LanguageContext);
 
   return (
-    <Card className="mb-5">
-      <Card.Header>
-        <FontAwesomeIcon icon={faList} className="me-2" />
-        {t("colorFormats.header")}
-      </Card.Header>
-      <ListGroup variant="flush">
-        <ListGroup.Item>
-          {t("colorFormats.hex")}:{" "}
-          <span className="color-code">{formats.hex}</span>
-        </ListGroup.Item>
-        <ListGroup.Item>
-          {t("colorFormats.rgb")}:{" "}
-          <span className="color-code">{formats.rgb}</span>
-        </ListGroup.Item>
-        <ListGroup.Item>
-          {t("colorFormats.hsl")}:{" "}
-          <span className="color-code">{formats.hsl}</span>
-        </ListGroup.Item>
-        <ListGroup.Item>
-          {t("colorFormats.cmyk")}:{" "}
-          <span className="color-code">{formats.cmyk}</span>
-        </ListGroup.Item>
-      </ListGroup>
-    </Card>
+    <>
+      <Card className="mb-5">
+        <Card.Header>
+          <FontAwesomeIcon icon={faList} className="me-2" />
+          {t("colorFormats.header")}
+        </Card.Header>
+        <ListGroup variant="flush">
+          <ListGroup.Item>
+            {t("colorFormats.hex")}:{" "}
+            <span className="color-code">{formats.hex}</span>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            {t("colorFormats.rgb")}:{" "}
+            <span className="color-code">{formats.rgb}</span>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            {t("colorFormats.hsl")}:{" "}
+            <span className="color-code">{formats.hsl}</span>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            {t("colorFormats.cmyk")}:{" "}
+            <span className="color-code">{formats.cmyk}</span>
+          </ListGroup.Item>
+        </ListGroup>
+      </Card>
+      <AdSenseBanner />
+    </>
   );
 }
